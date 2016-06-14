2722bbc7fa887d43200ee891c38f21af29d846fb
叶柳 <SiFooYee@gmail.com>
2016-06-14 10:56:42 +0800

    Fixed the problem about integrate package position for change

--- build_integrate.sh
+++ build_integrate.sh
@@ -4,7 +4,7 @@ source ../../build/envsetup.sh
 
 cs3
 
-cp -rf integrate/system/app/ overlay/system/app/
+cp -rf ../integrate/system/app/ overlay/system/app/
 echo ">>> copying integrate/system/app/ to overlay/system/app/"
 
 make clean-all
