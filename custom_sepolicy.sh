3385cb5bd6cb89ed52ae86148bdf44863c3affab
叶柳 <sifooyee@gmail.com>
2018-03-24 14:46:37 +0800

    Fixed an issue when building rom from other devices caused booting failed for lack of necessary directory

--- custom_sepolicy.sh
+++ custom_sepolicy.sh
@@ -4,7 +4,7 @@ SEPOLICY_INJECT=$1
 SEPOLICY=$2
 
 # Fix the issue about the "git" does not manage the empty folders.
-needCreatedDirs=("data" "dev" "oem" "proc" "sys" "system")
+needCreatedDirs=("acct" "cache" "config" "data" "dev" "mnt" "oem" "proc" "storage" "sys" "system")
 
 RAMDISK_DIR=`dirname $SEPOLICY`
 for dir_name in "${!needCreatedDirs[@]}"
